6429d4b675bc84923290519bae83800e795b945f
Shusnuke Oba <obashun22@yahoo.co.jp>
2024-06-04 22:00:21 +0900

    Update 雛形作成

---
+++ lib/screens/home_map_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeMapScreen extends StatefulWidget {
+  const HomeMapScreen({super.key});
+
+  @override
+  State<HomeMapScreen> createState() => _HomeMapScreenState();
+}
+
+class _HomeMapScreenState extends State<HomeMapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: null,
+      ),
+      body: null,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
