96c18d2e64150a3c2de90b8d8f6adb38027efc7a
Shusnuke Oba <obashun22@yahoo.co.jp>
2024-06-18 17:32:23 +0900

    Update Firebase初期化処理

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:rowing_navigator/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:rowing_navigator/screens/home_map_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Future<void> main() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
   runApp(const App());
 }
 
